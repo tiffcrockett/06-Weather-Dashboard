a53cc290aae020c09c29e9f719a1f4d970ba5888
Tiffany Crockett <crockett.tiff@gmail.com>
2020-12-24 16:27:47 -0700

    random push-partial format display

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,13 +18,20 @@ $(document).ready(function() {
 
                 $(".city").html("<h2>" + response.name + " Weather Details</h2>");
                 $("#current-date").text(new Date(response.dt).toLocaleDateString('en-US', {weekday: 'long'}));
-                $("#nowicon").attr("src",`http://openweathermap.org/img/wn/`) +response.weather.icon+"png"
+                $("#nowicon").attr("src",`http://openweathermap.org/img/wn/`) +response.weather.icon +"png"
                 $("#tempF").text("Temperature (F): " + response.main.temp);
                 $("#humidty").text("Humidity: " + response.main.humidity + " %");
                 $("#wind").text("Wind Speed: " + response.wind.speed + " MPH"); 
                 $("city-list").text(response.name);  
+
                 // $("#uvi").text("UV Index:" + response.current.uvi); 
 
+                // var lat = $("lat").val();
+                // var lon = $("lon").val();
+
+                // var queryURL2 = "httyp://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&cnt=5&units=imperial&appid=12453634a0c3823fc0a03953fcfff02b";    
+                // console.log(queryURL2)
+                
                 var queryURL2 = "http://api.openweathermap.org/data/2.5/forecast/daily/?q=" + city + "&units=imperial&cnt=5&appid=12453634a0c3823fc0a03953fcfff02b";           
                 console.log(queryURL2)
 
@@ -33,17 +40,25 @@ $(document).ready(function() {
                     method: "GET", 
                 }).then(function(response) { 
                     console.log(response) 
+                    console.log(response.list)  
 
-                    forecast = [];
+                    for(var i = 0; i < response.list.length; i++) {
 
-                    for (var i = 0; i < 5; i++) {
-                        $("#date").text(new Date(response.list[i].dt).toLocaleDateString('en-Us', {weekday: 'long'})), 
-                        $("#icon").attr("src",`http://openweathermap.org/img/wn/`) + response.list[i].weather[0].icon +"png";
-                        $("#temp").text(Math.round(response.list[i].temp.day) + " Degrees F");
-                        $("#humid").text(response.list[i].humidity + "% Humidity");
-                        
-                        $("#weatherForecast").push(date[i],icon[i],temp[i],humid[i]);
-                    }  
+                    var displayDiv = $("<div>").addClass("daily ");
+                    var card = $("<div>").addClass("card col-2 h-25 bg-primary text-white");
+                    var cardBody = $("<div>").addClass("card-body");
+                    var date = $("<h3>").addClass("card-text").text(new Date(response.list[i].dt).toLocaleDateString('en-Us', {weekday: 'long'}));
+                    var icon = $("<img>").attr("src", "http://openweathermap.org/img/w/") + response.list[i].weather[0].icon +"png";
+                    var temp = $("<div>").addClass("card-text").text(Math.round(response.list[i].temp.day) + " Degrees F");
+                    var humid = $("<div>").addClass("card-text").text(response.list[i].humidity);
+
+                    // * append elements to weatherForecast div
+                    cardBody.append(date, icon, temp, humid);
+                    card.append(cardBody);
+                    displayDiv.append(card);
+                    $("#weatherForecast").append(displayDiv)
+                    }
+            
                 });
 
             var newCity = $("input[name=listCity]").val();
@@ -64,5 +79,5 @@ $(document).ready(function() {
                 $("#uvi").css({"background-color":"#27ae60", "color":"#fff"})
             };  
         }); 
-    } displayWeather()
+    } displayWeather();
 });
