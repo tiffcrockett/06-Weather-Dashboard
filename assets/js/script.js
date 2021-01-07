5e39eb2baacc5e525410cbb713109aea3e0671e5
Tiffany Crockett <crockett.tiff@gmail.com>
2021-01-07 12:59:40 -0700

    update uvi

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,7 +29,7 @@ $(document).ready(function() {
 
         function displayWeather(city){
 
-            var queryURL = "http://api.openweathermap.org/data/2.5/weather/?q=" + city + "&units=imperial&appid=12453634a0c3823fc0a03953fcfff02b";
+            var queryURL = "https://api.openweathermap.org/data/2.5/weather/?q=" + city + "&units=imperial&appid=12453634a0c3823fc0a03953fcfff02b";
             
             $.ajax({ 
                 url:queryURL,
@@ -44,35 +44,33 @@ $(document).ready(function() {
                 $("#humidty").text("Humidity: " + response.main.humidity + " %");
                 $("#wind").text("Wind Speed: " + response.wind.speed + " MPH"); 
                 $("city-list").text(response.name);  
+    
+                var lat = response.coord.lat;
+                var lon = response.coord.lon;
 
-                // $("#uvi").text("UV Index:" + response.current.uvi); 
-
-                
-                // var lat = $("lat").val();
-                // var lon = $("lon").val();
-
-                // var queryURL3 = "httyp://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&cnt=5&units=imperial&appid=12453634a0c3823fc0a03953fcfff02b";    
-                // console.log(queryURL3)
-                
-                var queryURL2 = "http://api.openweathermap.org/data/2.5/forecast/daily/?q=" + city + "&units=imperial&cnt=6&appid=12453634a0c3823fc0a03953fcfff02b";           
+                var queryURL2 = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&cnt=6&units=imperial&appid=12453634a0c3823fc0a03953fcfff02b";    
                 console.log(queryURL2)
+                
+                // var queryURL2 = "https://api.openweathermap.org/data/2.5/forecast/daily/?q=" + city + "&units=imperial&cnt=6&appid=12453634a0c3823fc0a03953fcfff02b";           
+                // console.log(queryURL2)
 
                 $.ajax({ 
                     url:queryURL2,
                     method: "GET", 
                 }).then(function(response) { 
-                    console.log(response) 
-                    console.log(response.list)  
+                    console.log(response)  
+
+                    $("#uvi").text("UV Index: " + response.current.uvi); 
 
                     for(var i = 1; i < 6; i++) {
 
                     var displayDiv = $("<div>").addClass("daily");
                     var card = $("<div>").addClass("card bg-primary text-white");
                     var cardBody = $("<div>").addClass("card-body");
-                    var date = $("<h4></h4>").addClass("card-text").text(new Date(response.list[i].dt*1000).toLocaleDateString('en-Us', {weekday: 'long'}));
-                    var icon = $("<img>").attr("src","http://openweathermap.org/img/wn/" + response.list[i].weather[0].icon +".png");
-                    var temp = $("<div>").addClass("card-text").text(Math.round(response.list[i].temp.day) + " Degrees F");
-                    var humid = $("<div>").addClass("card-text").text(response.list[i].humidity + "% Humidity");
+                    var date = $("<h4></h4>").addClass("card-text").text(new Date(response.daily[i].dt*1000).toLocaleDateString('en-Us', {weekday: 'long'}));
+                    var icon = $("<img>").attr("src","http://openweathermap.org/img/wn/" + response.daily[i].weather[0].icon +".png");
+                    var temp = $("<div>").addClass("card-text").text(Math.round(response.daily[i].temp.day) + " Degrees F");
+                    var humid = $("<div>").addClass("card-text").text(response.daily[i].humidity + "% Humidity");
 
                     // * append elements to weatherForecast div
                     cardBody.append(date, icon, temp, humid);
@@ -82,7 +80,7 @@ $(document).ready(function() {
 
                     $("#current-weather").show();
                     $("five-day").show();
-                    };
+                    }; 
 
                 }); 
 
